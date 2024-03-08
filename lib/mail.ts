60416770119695db50dd13d632f0ef92c7070c95
enioaires <enio.aires@hotmail.com>
2024-03-08 11:44:59 -0300

    feat: new domain

--- lib/mail.ts
+++ lib/mail.ts
@@ -4,7 +4,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendTwoFactorTokenEmail = async (email: string, token: string) => {
   await resend.emails.send({
-    from: "onboarding@resend.dev",
+    from: "mail@maria-bonita.site",
     to: email,
     subject: "2FA Codigo de Verificacao",
     html: `<p>Seu codigo de segurança: ${token}</p>`,
@@ -15,7 +15,7 @@ export const sendPasswordResetEmail = async (email: string, token: string) => {
   const resetLink = `${process.env.BASE_URL}/auth/new-password?token=${token}`;
 
   await resend.emails.send({
-    from: "onboarding@resend.dev",
+    from: "mail@maria-bonita.site",
     to: email,
     subject: "Redefina sua senha",
     html: `<p>Clique <a href="${resetLink}">AQUI</a> para alterar sua senha</p>`,
@@ -26,7 +26,7 @@ export const sendVerificationEmail = async (email: string, token: string) => {
   const confirmLink = `${process.env.BASE_URL}/auth/new-verification?token=${token}`;
 
   await resend.emails.send({
-    from: "onboarding@resend.dev",
+    from: "mail@maria-bonita.site",
     to: email,
     subject: "Confirme seu e-mail",
     html: `<p>Clique <a href="${confirmLink}">AQUI</a> para confirmar seu e-mail</p>`,
